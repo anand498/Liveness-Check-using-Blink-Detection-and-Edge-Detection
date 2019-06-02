1c642e189b3057c13833b30b03b4bb87b2132116
anand498 <anand.pandey803@gmail.com>
2019-06-02 08:38:06 +0530

    Adding the files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Liveness-Check-using-Blink-Detection-and-Edge-Detection
+An implementation of face spoof detection using blink detection and edge detection.
---
+++ main.py
@@ -0,0 +1,157 @@
+import cv2
+import numpy as np
+import numpy
+import PIL
+import pandas as pd
+import logging as log
+from scipy.spatial import distance as dist
+from imutils.video import FileVideoStream
+from imutils.video import VideoStream
+from imutils import face_utils
+import argparse
+import imutils
+import time
+import dlib
+import sys
+import logging as log
+import datetime as dt
+from time import sleep
+from matplotlib import pyplot as plt
+from eye_aspect import eye_aspect_ratio
+
+
+ 
+EYE_AR_THRESH = 0.3
+EYE_AR_CONSEC_FRAMES = 3
+
+COUNTER = 0
+TOTAL = 0
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape/shape_predictor_68_face_landmarks.dat")
+
+# grab the indexes 
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+print("[INFO] starting video stream thread...")
+vs = FileVideoStream("blink_detection_demo.mp4").start()
+fileStream = True
+cascPath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+time.sleep(1.0)
+ij=10
+# loop over frames 
+while ij>0:
+	
+	if fileStream and not vs.more():
+		break
+
+	frame = vs.read()
+	frame = imutils.resize(frame, width=600)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	rects = detector(gray, 0)
+	
+	faces = faceCascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5,minSize=(30, 30))
+	for (x, y, w, h) in faces:
+    		cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+	for rect in rects:
+		shape = predictor(gray, rect)
+		shape = face_utils.shape_to_np(shape)
+		leftEye = shape[lStart:lEnd]
+		rightEye = shape[rStart:rEnd]
+		leftEAR = eye_aspect_ratio(leftEye)
+		rightEAR = eye_aspect_ratio(rightEye)
+
+		ear = (leftEAR + rightEAR) / 2.0
+		leftEyeHull = cv2.convexHull(leftEye)
+		rightEyeHull = cv2.convexHull(rightEye)
+		cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+		cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
+		if ear < EYE_AR_THRESH:
+			COUNTER += 1
+		else:
+			if COUNTER >= EYE_AR_CONSEC_FRAMES:
+				TOTAL += 1
+
+			COUNTER = 0
+		if TOTAL>0:
+			cv2.putText(frame, "Live!", (10, 30),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+		else:
+			cv2.putText(frame, "Fake!", (10, 30),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+		ij-=1
+		
+	
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+	if key == ord("q"):
+		break
+
+
+
+vidcap = cv2.VideoCapture(0)
+#vidcap = cv2.VideoCapture('blink_detection_demo2.mp4')
+success,image = vidcap.read()
+cascPath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+count1 = 0
+countchanges=0
+count2=10
+counter=0
+while(count2>0):
+    success,image = vidcap.read()
+    print('Read a new frame: ', success)
+    cv2.imwrite("frame%d.jpg" % count1, image)
+    
+    img = cv2.imread('frame%d.jpg'%count1)
+    if(count2==9):
+       cv2.imshow("Frame", img)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
+    )
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    kernel = np.ones((15,15),np.uint8)
+    dilate = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, 3)
+    contours,hierarchy = cv2.findContours(dilate,2,1)
+    cv2.drawContours(img, contours, -1, (0,255,0), 3)
+    cv2.imwrite("thresh%d.jpg" % countchanges, thresh)
+    countchanges+=1
+    img = PIL.Image.open("thresh0.jpg").convert("L")
+    imgarr = numpy.array(img)
+    imgarr[imgarr > 128] = 255
+    imgarr[imgarr < 128]= 0
+    a=imgarr[250,:]
+    cv2.imshow('frame',image)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    count2-=1
+
+
+for index,k in enumerate(a):
+       if index+1 < len(a):
+         if k != a[index+1]:
+          count1+=1
+print("The count is ")
+print(count1)
+if(count1>5):
+  print("This is a Fake person")
+else:
+  print("This is a real person")
+
+
+cv2.destroyAllWindows()
+vs.stop()
